eaad10443921314c443c9b58275f6ebb4655f284
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2024-10-05 14:02:41 +0200

    add components and pages

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header/Header'
+import HomePage from './pages/HomePage/HomePage'
+import MoviesPage from './pages/MoviesPage/MoviesPage'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/movies' element={<MoviesPage />} />
+        <Route path='*' element={<NotFoundPage/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
